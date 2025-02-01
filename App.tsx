59077df052ec6ccbe24dda93bb14efb5a9b501cf
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-02-01 02:32:28 +0100

    Refactor App component layout and add theme file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+ export default function App() {
+   return (
+     <View style={styles.container}>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+       </View>
+     </View>
+   );
+  }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    container: {
+      flex: 1,
+     backgroundColor: "#fff",
+     justifyContent: "center",
+   },
+  itemContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    borderBottomColor: "#1a759f",
+    borderBottomWidth: 1,
   },
-});
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+ });
