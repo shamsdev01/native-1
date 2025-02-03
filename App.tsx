48d54e9a2e9069286e70d253a713352d33d60b6c
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-02-03 01:28:14 +0100

    Add delete functionality and update theme colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,Alert, Button, TouchableOpacity, Pressable } from "react-native";
+import { theme } from "./theme";
+
+
+
  export default function App() {
+
+  // Delete button Function
+  const handleDelete = () => {
+    Alert.alert("Are you sure you want to delete this item?","It will be gone Forever",[
+      {
+        text: "yes",
+        onPress: () => console.log("Item deleted"),
+      },
+      {
+        text: "cancel", 
+        onPress: () => console.log("Item not deleted"),
+      },
+    ]);
+  };
+
    return (
      <View style={styles.container}>
 
+      {/* <Button title="Press me" onPress={() => alert("Button pressed")} /> */}
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+         style={styles.button}
+         onPress={handleDelete}
+         >
+     <Text style={styles.buttonText}>Delete</Text>
+     </TouchableOpacity>
        </View>
      </View>
    );
@@ -20,9 +46,24 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
     borderBottomColor: "#1a759f",
     borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+   justifyContent: "space-between",
   },
   itemText: {
     fontSize: 18,
     fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: { 
+    color: theme.colorWhite,
+    fontSize: 16,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
  });
--- theme.ts
+++ theme.ts
@@ -1,5 +1,6 @@
 export const theme ={
     colorCerulean: '#1a759f',
     colorWhite: '#f0f0f0',
+    colorBlack: '#000000',
 };
  
