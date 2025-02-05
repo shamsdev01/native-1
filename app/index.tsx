0b7c9bfdf187d56ada7bac1a5c91b9654a9a0677
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-02-05 13:15:32 +0100

    Add last updated timestamp to shopping list items and implement sorting logic

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,7 @@ type ShoppingListItemType = {
   id: string;  
   name: string;
   completedAtTimestamp?: number;
+  lastUpdatedTimestamp: number;
 }
 
  
@@ -19,7 +20,8 @@ type ShoppingListItemType = {
    const handleSubmit = () => {
      if (value) { 
        const newShoppingList = [
-         { id: new Date().toISOString(), name: value },
+         { id: new Date().toISOString(), name: value, 
+          lastUpdatedTimestamp: Date.now() },
          ...shoppingList,
        ];
        setShoppingList(newShoppingList);
@@ -38,6 +40,7 @@ type ShoppingListItemType = {
         if (item.id === id) {
           return {
             ...item,
+            lastUpdatedTimestamp: Date.now(),
             completedAtTimestamp: item.completedAtTimestamp ? undefined : Date.now(),
           };
         }
@@ -48,7 +51,7 @@ type ShoppingListItemType = {
 
    return (
     <FlatList
-    data={shoppingList}
+    data={orderShoppingList (shoppingList)}
     style={styles.container}
     ListEmptyComponent={
       <View style={styles.listEmptyContainer}>
@@ -77,6 +80,33 @@ type ShoppingListItemType = {
     />
    );
   }
+
+
+  function orderShoppingList(shoppingList: ShoppingListItemType[]) {
+    return shoppingList.sort((item1, item2) => {
+      if (item1.completedAtTimestamp && item2.completedAtTimestamp) {
+        return item2.completedAtTimestamp - item1.completedAtTimestamp;
+      }
+  
+      if (item1.completedAtTimestamp && !item2.completedAtTimestamp) {
+        return 1;
+      }
+  
+      if (!item1.completedAtTimestamp && item2.completedAtTimestamp) {
+        return -1;
+      }
+  
+      if (!item1.completedAtTimestamp && !item2.completedAtTimestamp) {
+        return item2.lastUpdatedTimestamp - item1.lastUpdatedTimestamp;
+      }
+  
+      return 0;
+    });
+  }
+
+
+
+  
 const styles = StyleSheet.create({
     container: {
       flex: 1,
